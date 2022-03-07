90b25c4332a3583b7bd60b72cb0684f99029853d
ismail26 <iguven1999@hotmail.com>
2022-03-07 10:48:27 +0300

    update

--- consumer.js
+++ consumer.js
@@ -1,4 +1,6 @@
 const amqp = require("amqplib");
+const data = require('./data.json');
+const queueName = process.argv[2] || "jobsQueue";
 
 connect_rabbitmq();
 
@@ -6,11 +8,17 @@ async function connect_rabbitmq() {
     try {
         const connection = await amqp.connect("amqp://localhost:5672");
         const channel = await connection.createChannel();
-        const assertion = await channel.assertQueue("jobsQueue");
+        const assertion = await channel.assertQueue(queueName);
 
         // Receive message
-        channel.consume("jobsQueue", message => {
-            console.log("Received message ->", message.content.toString());
+        console.log("Waiting for message...");
+        channel.consume(queueName, message => {
+            const messageInfo = JSON.parse(message.content.toString());
+            const userInfo = data.find(u => u.id === messageInfo.description);
+            if(userInfo){
+                console.log("Processed, ",userInfo);
+                channel.ack(message);
+            }
         });
 
     } catch (error) {
--- publisher.js
+++ publisher.js
@@ -1,4 +1,7 @@
 const amqp = require("amqplib");
+const data = require("./data.json");
+const queueName = process.argv[2] || "jobsQueue";
+
 
 const message = {
     description: "Lorem ipsum dolor sit amet."
@@ -6,15 +9,27 @@ const message = {
 
 connect_rabbitmq();
 
-async function connect_rabbitmq(){
+async function connect_rabbitmq() {
     try {
         const connection = await amqp.connect("amqp://localhost:5672");
-        const channel = await connection.createChannel();  
-        const assertion = await channel.assertExchange("jobsQueue");
-        
-        channel.sendToQueue("jobsQueue",Buffer.from(JSON.stringify(message)));
-        console.log("Sended Message ->",message);
+        const channel = await connection.createChannel();
+        const assertion = await channel.assertExchange(queueName);
+
+        data.forEach(i=>{
+            message.description = i.id;
+            channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)));
+            console.log("Sended Message ->", i.id);
+        });
+       
+
+        /////////// INTERVAL ///////////////////
+        // setInterval(()=>{
+        //     message.description =  new Date().getTime();
+        //     channel.sendToQueue(queueName,Buffer.from(JSON.stringify(message)));
+        //     console.log("Sended Message ->",message);
+        // },10);
+
     } catch (error) {
-        console.log("Error: ",error)
+        console.log("Error: ", error)
     }
 }
